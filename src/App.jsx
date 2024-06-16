a0b7048f0497e330e4bad0bb97a4d52acf4d4809
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-17 01:06:30 +0530

    creating route in React Apk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  
   return (
     <div>
-      
+      <h1>Hello Router</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      Not found! :(
+    </div>
+  )
+}
