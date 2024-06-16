52ac3c30b9abf4db5d89a1c4c50bddde6e4eb8f4
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-17 02:12:07 +0530

    creating appLayout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  )
+}
