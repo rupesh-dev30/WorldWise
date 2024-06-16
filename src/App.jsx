93b99d0dcabc3840dc311457761489f05a6ac078
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-17 02:18:15 +0530

    outlet (Route during non component passing)

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login"
-import "./index.css"
+import Login from "./pages/Login";
+import "./index.css";
 
 export default function App() {
   return (
@@ -14,7 +14,11 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>List of Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,16 @@
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
+
+      <Outlet />
+      
       <p>List of cities</p>
 
       <footer className={styles.footer}>
