52e055a769573f7275f5c44361ccde6b84deb105
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-17 23:10:40 +0530

    adding Countries Detail

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,6 +7,7 @@ const BASE_URL = "http://localhost:9000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isloading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -24,11 +25,26 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error in loading data!!");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isloading,
+        currentCity,
+        getCity
       }}
     >
       {children}
@@ -36,10 +52,11 @@ function CitiesProvider({ children }) {
   );
 }
 
-function useCities(){
+function useCities() {
   const context = useContext(CitiesContext);
-  if(context === undefined) throw new Error("CitiesContext was used outside the CitiesProvider")
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
   return context;
 }
 
-export {CitiesProvider, useCities};
+export { CitiesProvider, useCities };
