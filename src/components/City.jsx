fe72c3969ff9989480dd2a05d42c857c1a25259e
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-17 23:24:47 +0530

    making new BackButton component

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,8 +1,9 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -27,7 +28,7 @@ function City() {
   const { cityName, emoji, date, notes } = currentCity;
 
   if(isLoading) return Spinner;
-  
+
   return (
     <div className={styles.city}>
       <div className={styles.row}>
@@ -61,7 +62,7 @@ function City() {
       </div>
 
       <div>
-        {/* <ButtonBack /> */}
+        <BackButton />
       </div>
     </div>
   );
