433349ce5c26bc1b3a2b969ec28ff4a9ef8bf3f6
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-19 00:14:57 +0530

    update

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -2,11 +2,12 @@ import { createContext, useContext } from "react";
 
 const AuthContext = createContext();
 
-function AuthProvider({children}){
-  return <AuthContext.Provider>{children}</AuthContext.Provider>
+function AuthProvider({ children }) {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
 }
 
-function useAuth(){
+function useAuth() {
   const context = useContext(AuthContext);
-  if(context === undefined) throw new Error("AuthContext was used outside AuthProvider");
+  if (context === undefined)
+    throw new Error("AuthContext was used outside AuthProvider");
 }
